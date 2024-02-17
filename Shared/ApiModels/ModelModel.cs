e984cc901bd221db2143c967273d8a6030b21a19
lapizzamolo <lerouxmarin7@gmail.com>
2024-02-17 23:20:17 +0100

    Add ApiModel + SharedProject

---
+++ Shared/ApiModels/ModelModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.ApiModels
+{
+    public class ModelModel
+    {
+        public class Model
+        {
+            public string Brand { get; set; }
+            public int MaintenanceFrequency { get; set; }
+            public string Description { get; set; }
+        }
+    }
+}
