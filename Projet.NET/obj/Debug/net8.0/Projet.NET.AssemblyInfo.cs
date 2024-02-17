d4919a798b32c8c7f0251212a929946b5f2c1c6a
bastien mioche <bastien.mioche@cgi.com>
2024-02-17 21:48:45 +0100

    2_Dependancy

--- Projet.NET/obj/Debug/net8.0/Projet.NET.AssemblyInfo.cs
+++ Projet.NET/obj/Debug/net8.0/Projet.NET.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projet.NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+982de13872088efdad00f47b2cd919574198532b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projet.NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projet.NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
